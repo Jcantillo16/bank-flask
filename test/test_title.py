80917d20e80ba1aa971cd1f2813e8bb3f46ac366
juan_cantillo <juan.cantillo@leanware.io>
2022-12-26 21:13:55 -0500

    :test_tube: add test

--- test/test_title.py
+++ test/test_title.py
@@ -9,23 +9,16 @@ db = get_dal().get_db()
 
 class TestTitleService(unittest.TestCase):
     def test_get_cantidad_titulos(self):
-        # Arrange
         service = TitleService()
-        # Act
         cantidad = service.get_cantidad_titulos(db)
-        # Assert
         self.assertEqual(cantidad, 3)
 
     def test_delete_titulo(self):
-        # Arrange
         service = TitleService()
-        # Act
         titulo = service.delete_titulo(db, ID_TITULOS[0])
-        # Assert
         self.assertEqual(titulo, None)
 
     def test_create_titulo(self):
-        # Arrange
         service = TitleService()
         nuevo_titulo = Title(
             id_titulo='THI',
@@ -36,21 +29,14 @@ class TestTitleService(unittest.TestCase):
             fecha_vencimiento='2021-01-01',
             pago_cuota='y',
         )
-        # Act
         titulo = service.create_titulo(db, nuevo_titulo)
-        # Assert
         self.assertEqual(titulo, nuevo_titulo)
 
     def test_get_titulos(self):
-        # Arrange
         service = TitleService()
-        # Act
         titulos = service.get_titulos(db)
-        # Assert
         self.assertEqual(db, titulos)
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
